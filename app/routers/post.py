68728a5655cdd003a4dcce3da82bff5910ea8810
komsnega <114226365+komsnega@users.noreply.github.com>
2024-01-20 14:28:46 +0300

    Update post.py
    Extra prints added into create_posts func to investigate issue faced in render

--- app/routers/post.py
+++ app/routers/post.py
@@ -35,21 +35,15 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_posts(post:schemas.PostCreate, db: Session = Depends(get_db), 
                  current_user: int = Depends(oauth2.get_current_user)):
-# w/out sqlalchemy:
-#def create_posts(post: Post):
-    # with conn.cursor() as cur:
-    #     cur.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #             (post.title, post.content, post.published))
-    #     new_post = cur.fetchone()
-    # conn.commit()
-    #new_post=models.Post(title=post.title, content=post.content, published=post.published)
-    print(current_user.email)
+    print("Inside create_posts function")
+    print(f"Current user: {current_user.email}")
     new_post=models.Post(user_id=current_user.user_id, **post.model_dump())
+    print(f"New post: {new_post}")
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
+    print(f"Post after commit: {new_post}")
     return new_post
- 
 
 @router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id:int, db: Session = Depends(get_db)):
